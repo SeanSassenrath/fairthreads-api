2f84d5b8f2c7fb6d0f591ab6ef50dfe96a3c1a50
Alexandra Estrada <estrada.ae@gmail.com>
2016-02-21 22:09:11 -0800

    add api route to display all products

--- server.js
+++ server.js
@@ -11,8 +11,23 @@ mongoose.connect('mongodb://localhost:27017/fairthreads');
 
 // logs all requests to the console
 app.use(morgan('dev'));
-
 shopStyleApi();
 
+var apiRouter = express.Router();
+apiRouter.use(function(req,res,next) {
+  console.log('somebody just came to our app!')
+  next();
+})
+apiRouter.get('/', function(req,res) {
+  products.find(function(err,products) {
+    if(err) {
+      res.send(err);
+    }
+    else {
+      res.json(products)
+    }
+  })
+})
+app.use('/api', apiRouter);
 app.listen(config.port);
 console.log("FairThread API is live on " + config.port);
