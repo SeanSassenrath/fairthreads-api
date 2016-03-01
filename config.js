647ff98138b0a80dea83660f8ca26227e5dc7a51
SeanSassenrath <sean.sassenrath@gmail.com>
2016-02-29 20:12:47 -0800

    Config for herkou

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+ 'port': process.env.PORT || 9000,
+ 'shopstyleAPIKey': 'uid100-33047490-67',
+ 'mongoLabURI': 'mongodb://heroku_52k3gzwv:5env2kro1te4ilkucu3cca484v@ds015478.mongolab.com:15478/heroku_52k3gzwv/fairthreads',
+ 'mongoLocal': 'mongodb://localhost:27017/fairthreads'
+}
