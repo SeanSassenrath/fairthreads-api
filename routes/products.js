accdfc4b1003f9116fed3280ea239e1fe1c728f1
SeanSassenrath <sean.sassenrath@gmail.com>
2016-04-09 22:35:57 -0700

    Add categoryList to products

--- routes/products.js
+++ routes/products.js
@@ -4,13 +4,29 @@ module.exports = function(app, express) {
   var productsRouter = express.Router();
 
   productsRouter.use(function(req, res, next) {
-    console.log("API was just hit by", req)
+    // console.log("API was just hit by", req)
     next()
   })
 
   productsRouter.get('/gender/:gender', function(req, res) {
-    Product.find({gender: req.params.gender}, function(err, products){
-      err ? res.send(err) : res.json(products)
+    var products = {};
+
+    var assignCategories = function(items) {
+      var categories = [];
+        items.forEach(function(item) {
+          if(categories.indexOf(item.fairThreadsCategory) < 0 && item.fairThreadsCategory != undefined) categories.push(item.fairThreadsCategory)
+        })
+      return categories;
+    }
+
+    Product.find({gender: req.params.gender}, function(err, productsByGender){
+      if(err) {
+        res.send(err)
+      } else {
+        products.items = productsByGender;
+        products.categoryList = assignCategories(productsByGender);
+        res.json(products);
+      }
     })
   })
 
