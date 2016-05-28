1cec81de91d9bcc579ac8a125b9b3398f35703aa
SeanSassenrath <sean.sassenrath@gmail.com>
2016-05-27 22:25:58 -0700

    Add more properties to the product model

--- models/product.js
+++ models/product.js
@@ -4,21 +4,25 @@ var mongoose = require('mongoose');
 var Schema =  mongoose.Schema;
 
 var ProductSchema = new Schema({
-  shopstyleId: {type:Number, required: true, unique: true},
-  name: {type:String,required: true},
-  brand: {type:String,required: true},
-  price: {type:Number,min:0, required:true},
-  salePrice: {type:Number},
-  vendUrl: {type:String, required:true},
-  imageLarge: {type:String, required: true},
-  imageSmall: {type:String, required: true},
-  imageOriginal: {type:String, required: true},
-  category: {type:String, required: true},
+  shopstyleId: {type: Number, required: true, unique: true},
+  name: {type: String,required: true},
+  brand: {type: String,required: true},
+  price: {type: Number,min:0, required:true},
+  salePrice: {type: Number},
+  vendUrl: {type: String, required:true},
+  imageLarge: {type: String, required: true},
+  imageSmall: {type: String, required: true},
+  imageOriginal: {type: String, required: true},
+  category: {type: String, required: true},
   fairThreadsCategory: {type: String},
-  description: {type:String},
-  color: {type:String},
-  gender: {type:String},
-  softDelete: {type: Boolean, required:true, default: false}
+  description: {type: String},
+  color: {type: String},
+  gender: {type: String},
+  softDelete: {type: Boolean, required:true, default: false},
+  active: {type: Boolean, default: false},
+  activeTimeStamp: {type: Date},
+  style: {type: String},
+  categoryRank: {type: Number}
 })
 
 var Product = mongoose.model('Products', ProductSchema);
