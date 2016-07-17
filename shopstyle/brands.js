12aef2fd48745eb8d1957aa2bb82e8b1e3823b7e
SeanSassenrath <sean.sassenrath@gmail.com>
2016-07-16 23:18:35 -0700

    Add more brands

--- shopstyle/brands.js
+++ shopstyle/brands.js
@@ -10,6 +10,23 @@ module.exports = {
     "b5506", //nudie jeans
     "b31087", //flynn skye
     "b32078", //base range
-    "b7907" //People Tree
+    "b7907", //People Tree
+    "b32078", //Base Range
+    "b31641", //Cp Shades
+    "b31087", //Flynn Skye
+    "b31969", //Threads 4 Thought
+    "b14524", //Alternative Apperal
+    "b5573", //Brother Vellies
+    "b31937", //Maiyet
+    "b2034", //Stella McCartney
+    "b192", //Eileen Fisher
+    "b29731", //Araks
+    "b189", //Edun
+    "b29377", //Chinti & Parker
+    "b31623", //Amour Vert
+    "b26183", //Prana
+    "b6937", //Fortress of Inca
+    "b14903", //Prairie Underground
+    "b8029", //Pons Avaracas
   ]
 }
