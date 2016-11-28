d1e938bc31330bba36bde665878e6b91b8c8606a
SeanSassenrath <sean.sassenrath@gmail.com>
2016-11-27 22:42:34 -0800

    Allow put requests

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // CORS
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Methods', 'GET', 'PUT');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
   next();
 })
