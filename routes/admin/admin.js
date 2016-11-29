3dc950ab362a57d802cda61b5aa97d3865285966
SeanSassenrath <sean.sassenrath@gmail.com>
2016-11-28 23:23:15 -0800

    Stringify id

--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -160,7 +160,7 @@ module.exports = function(app, express) {
         console.log('request --- ', req.body)
         var id = req.body.id;
         console.log('id --- ', id)
-        products.find(ObjectId(id), function(err, product) {
+        products.find(ObjectId(id.toString()), function(err, product) {
           if (err) res.send(err);
 
           item.softDelete = true;
