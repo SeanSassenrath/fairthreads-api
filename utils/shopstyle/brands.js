5f1ba3ba75419d920a189610927dd79f485ba427
Sean Sassenrath <sean.sassenrath@gmail.com>
2017-03-12 13:16:47 -0700

    Add brands backs

--- utils/shopstyle/brands.js
+++ utils/shopstyle/brands.js
@@ -2,6 +2,27 @@ module.exports = {
   brandsById: [
     'b22931', //Reformation
     'b7768', //Kuyichi
+    'b14084', //matt & nat
+    'b18079', //veja
+    'b7907', //People Tree
+    'b32078', //Base Range
+    'b31641', //Cp Shades
+    'b31087', //Flynn Skye
+    'b14524', //Alternative Apperal
+    'b5573', //Brother Vellies
+    'b31937', //Maiyet
+    'b29731', //Araks
+    'b189', //Edun
+    'b29377', //Chinti & Parker
+    'b31623', //Amour Vert
+    'b14903', //Prairie Underground
+    'b2034', //Stella McCartney
+    'b30363', //Everlane
+    'b5506', //nudie jeans
+    'b31969', //Threads 4 Thought
+    'b26183', //Prana
+    'b6287', //Patagonia
+    'b3284', //Sorel
   ],
   brandsBySearch: [
     'ecoalf',
