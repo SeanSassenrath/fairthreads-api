06572321acc83420ee49720d17dfb432e5c4aad6
Sean Sassenrath <sean.sassenrath@gmail.com>
2017-03-12 22:09:06 -0700

    Fix clean-db helper

--- utils/db-helpers/clean-db.js
+++ utils/db-helpers/clean-db.js
@@ -2,28 +2,18 @@ const Product = require('../../models/product');
 
 const deletedProducts = [];
 const now = new Date();
-const twoDays = (1000 * 60 * 60 * 24 * 2);
-const updateCutOff = now - twoDays;
+const twoDays = (1000 * 60 * 60 * 24 * 3);
+const updateCutOff = new Date(now - twoDays);
+console.log('--- updateCutOff', updateCutOff);
 
 const cleanDb = () => {
-  Product.find({ updatedAt: { $lt: updateCutOff } })
-    .then((product) => {
-      if (product.id) {
-        Product.findOneAndRemove({ id: product.id })
-        .then((deletedProduct) => {
-          console.log(`--- Deleting ${deletedProduct.details.name}`);
-          deletedProducts.push(product);
-        })
-        .catch((err) => {
-          console.log('ERROR', err);
-        });
-      } else {
-        console.log('--- No products to delete');
-      }
-    })
-    .then(() => {
-      console.log(`--- Deleted ${deletedProducts.length}`);
-    })
+  Product.remove({ updatedAt: { $lte: new Date(updateCutOff) } })
+    .then(products => (
+      console.log('products', products)
+      // products.map(product => (
+      //   console.log('product', product.updateAt)
+      // ))
+    ))
     .catch((err) => {
       console.log('ERROR', err);
     });
