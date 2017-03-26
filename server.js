6bcde45a209b7e60f7a8904935c8d15c355a5524
Sean Sassenrath <sean.sassenrath@gmail.com>
2017-03-25 23:53:37 -0700

    Adjust CORS options

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(methodOverride((req, res) => {
   }
 }));
 
-pullProducts();
+// pullProducts();
 // cleanDb();
 
 const options = {
@@ -45,10 +45,11 @@ if (process.env.NODE_ENV !== 'test') {
 }
 
 // CORS
+
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, PUT');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
